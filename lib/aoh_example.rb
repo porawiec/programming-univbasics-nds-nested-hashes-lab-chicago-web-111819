a444d2d071fe30f12580dde279ca7cfe4dd30b03
Paul O <paul.orawiec@gmail.com>
2019-10-30 08:43:53 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,7 +17,8 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   infinite_jest = [
-      {
+      {name:  "Don Gately", occupation: "Live-in Staff"},
+      
     ]
 end
 
